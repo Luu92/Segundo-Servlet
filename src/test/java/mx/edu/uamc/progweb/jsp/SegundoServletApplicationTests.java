e41050f16133cb73bee6fe80097c3dba9c33b751
DevLuu <luu.vale.05@gmail.com>
2021-10-12 02:05:36 -0500

    mi primer commit

---
+++ src/test/java/mx/edu/uamc/progweb/jsp/SegundoServletApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uamc.progweb.jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
